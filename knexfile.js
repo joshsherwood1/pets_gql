5a4deeea69c688f88e9563e9c684f9d3ea85de89
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-12-30 16:18:12 -0700

    Update knexfile with production information

--- knexfile.js
+++ knexfile.js
@@ -14,4 +14,15 @@ module.exports = {
     useNullAsDefault: true,
   },
 
+  production: {
+    client: 'pg',
+    connection: 'postgres://nnzyruqxaalmli:45b374a5633096c33743b04d3626617d2617e96967e791628a010df13fbf99a3@ec2-174-129-33-196.compute-1.amazonaws.com:5432/dalf3khq3muke5',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  }
 };
